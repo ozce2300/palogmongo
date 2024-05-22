3248314a00991b675a96a6f35a9f58a0cc11e943
Özgur Celik <ozce2300@student.miun.se>
2024-05-22 19:45:50 +0200

    Token tillämpad

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken")
 require("dotenv").config();
 
 //Connect to MongoDB
@@ -61,7 +62,14 @@ router.post("/login", async (req,res) =>{
         if(!isPasswordMatch) {
          return res.status(401).json({error: "Incorrect username/password"})
         } else {
-            res.status(200).json({message: "User logged in!"})
+            //create jwt
+            const payload = {username:username};
+            const token = jwt.sign(payload, process.env.JWT_SECRET_KEY, {expiresIn: '1h'});
+           const response = {
+            message: "User logged in",
+            token:token
+           }
+            res.status(200).json({response})
         }
 
 
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require("express");
 const authRoutes = require("./routes/authRoutes");
 const bodyParser = require("body-parser");
 require("dotenv").config();
+const jwt = require("jsonwebtoken")
 
 const port = process.env.PORT || 3400
 const app = express();
@@ -11,6 +12,25 @@ app.use(bodyParser.json());
 
 //Routes
 app.use("/api", authRoutes)
+
+//Protected routes
+app.get("/api/protected", authenticateToken, (req,res) =>{
+    res.json({message:"skyddat route"})
+});
+
+//Validate token
+function authenticateToken(req,res,next) {
+    const authHeader = req.headers[`authorization`];
+    const token = authHeader && authHeader.split(" ")[1]
+    if(token==null) res.status(401).json({message: "Not authorized for this route"});
+
+    jwt.verify(token, process.env.JWT_SECRET_KEY, (err, username) => {
+        if(err) return res.status(403).sjon({error: "Not correct jwt"});
+
+        req.username = username;
+        next();
+    })
+}
 
 
 //Start application
