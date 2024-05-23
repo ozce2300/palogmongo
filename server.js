4596b522028c02d110e96814e3e2df721fd65bca
Özgur Celik <ozce2300@student.miun.se>
2024-05-23 10:43:30 +0200

    rätta till fel i server.js

--- server.js
+++ server.js
@@ -1,42 +1,41 @@
-//Skriven av Özgur
 
+//Skriver av Özgur
 const express = require("express");
 const authRoutes = require("./routes/authRoutes");
 const bodyParser = require("body-parser");
 require("dotenv").config();
-const jwt = require("jsonwebtoken")
+const jwt = require("jsonwebtoken");
 const cors = require("cors");
 
-const port = process.env.PORT || 3400
+const port = process.env.PORT || 3400;
 const app = express();
 app.use(cors());
 
 app.use(bodyParser.json());
 
-//Routes
-app.use("/api", authRoutes)
+// Routes
+app.use("/api", authRoutes);
 
-//Protected routes
-app.get("/api/protected", authenticateToken, (req,res) =>{
-    res.json({message:"skyddat route"})
+// Protected routes
+app.get("/api/protected", authenticateToken, (req, res) => {
+    res.json({ message: "skyddat route", user: req.user });
 });
 
-//Validate token
-function authenticateToken(req,res,next) {
-    const authHeader = req.headers[`authorization`];
-    const token = authHeader && authHeader.split(" ")[1]
-    if(token==null) res.status(401).json({message: "Not authorized for this route"});
+// Validate token
+function authenticateToken(req, res, next) {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(" ")[1];
+    if (token == null) return res.status(401).json({ message: "Not authorized for this route" });
 
-    jwt.verify(token, process.env.JWT_SECRET_KEY, (err, username) => {
-        if(err) return res.status(403).sjon({error: "Not correct jwt"});
+    jwt.verify(token, process.env.JWT_SECRET_KEY, (err, user) => {
+        if (err) return res.status(403).json({ error: "Not correct jwt" });
 
-        req.username = username;
+        req.user = user;
         next();
-    })
+    });
 }
 
-
-//Start application
+// Start application
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`)
+    console.log(`Server running on port ${port}`);
 });
